d754b0bc31a91702bb353cdfd955fce46c3eb506
Paweł Szynal <pawel@szynal.info>
2022-04-29 00:19:06 +0200

    Added of github image

--- lablize.py
+++ lablize.py
@@ -4,32 +4,33 @@ from nltk.tbl.template import Template
 from nltk.tag.brill import Pos, Word
 from nltk.tag import untag, RegexpTagger, BrillTaggerTrainer, UnigramTagger
 
-def main():
 
+def main():
     with open('text_data/screenshot_20220329_193633.txt', 'r') as f:
         text = f.read()
         labeled_text = preprocess_text(text)
-        k = math.floor(len(labeled_text)/4)
+        k = math.floor(len(labeled_text) / 4)
         # print(labeled_text)
         training_data = labeled_text[:k]
-        baseline_data = labeled_text[k:3*k]
-        gold_data = list(labeled_text[3*k:])
+        baseline_data = labeled_text[k:3 * k]
+        gold_data = list(labeled_text[3 * k:])
         # print(gold_data)
         testing_data = [untag([s]) for s in gold_data]
         backoff = RegexpTagger([
-            (r'^-?[0-9]+(.[0-9]+)?$', 'CD'),   # cardinal numbers
-            (r'(The|the|A|a|An|an)$', 'AT'),   # articles
-            (r'.*able$', 'JJ'),                # adjectives
-            (r'.*ness$', 'NN'),                # nouns formed from adjectives
-            (r'.*ly$', 'RB'),                  # adverbs
-            (r'.*s$', 'NNS'),                  # plural nouns
-            (r'.*ing$', 'VBG'),                # gerunds
-            (r'.*ed$', 'VBD'),                 # past tense verbs
-            (r'.*', 'NN')                      # nouns (default)
-            ])
+            (r'^-?[0-9]+(.[0-9]+)?$', 'CD'),  # cardinal numbers
+            (r'(The|the|A|a|An|an)$', 'AT'),  # articles
+            (r'.*able$', 'JJ'),  # adjectives
+            (r'.*ness$', 'NN'),  # nouns formed from adjectives
+            (r'.*ly$', 'RB'),  # adverbs
+            (r'.*s$', 'NNS'),  # plural nouns
+            (r'.*ing$', 'VBG'),  # gerunds
+            (r'.*ed$', 'VBD'),  # past tense verbs
+            (r'.*', 'NN')  # nouns (default)
+        ])
 
         unigram_tagger = UnigramTagger([baseline_data], backoff=backoff)
         print(unigram_tagger.accuracy([gold_data]))
+
 
 def preprocess_text(text):
     """
